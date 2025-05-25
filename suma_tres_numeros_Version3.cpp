070f3f347a786cfa8759e98384bef0a4864a2da8
unknown <samuelsg2109@gmail.com>
2025-05-25 01:15:25 -0400

    LAB 1 suma de tres numeros

---
+++ suma_tres_numeros_Version3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // Declarar variables para almacenar los tres números
+    double num1 = 53;
+    double num2 = -27;
+    double num3 = 0.005;
+    double suma;
+
+    // Calcular la suma
+    suma = num1 + num2 + num3;
+
+    // Mostrar el resultado
+    std::cout << "La suma de los tres números es: " << suma << std::endl;
+
+    return 0;
+}
